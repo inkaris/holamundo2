1ae46b27a3a12b97064ad01b85f735e00eafd4b8
ionbastida <ionbastida@alumno-desktop>
2015-04-23 13:09:48 +0200

    metodo creado

--- exposiciondeed/src/Holamundo/holamundo.java
+++ exposiciondeed/src/Holamundo/holamundo.java
@@ -4,6 +4,9 @@ public class holamundo {
 	public static void main(String[] args) {
 		System.out.println("hola DAM!!!!");
 		System.out.println("");
-		System.out.println("Espacio en blanco");
+		System.out.println("E");
+	}
+	public static void eresUnaZorra(){
+		System.out.println("Si");
 	}
 }
