6b69ef924902f1b66111f4e266678036926942ba
ionbastida <ionbastida@alumno-desktop>
2015-04-23 13:23:09 +0200

    holamundo casi terminado

--- exposiciondeed/src/Holamundo/holamundo.java
+++ exposiciondeed/src/Holamundo/holamundo.java
@@ -1,9 +1,32 @@
-package Holamundo;
+package ejercicio3;
 
-public class holamundo {
-	public static void main(String[] args) {
-		System.out.println("hola DAM!!!!");
-		System.out.println("");
-		System.out.println("E");
-	}
-}
+import ejercicio3.Desempleado;
+import ejercicio3.EstudianteCCFF;
+import ejercicio3.Formable;
+import ejercicio3.Persona;
+
+public class DarFormacion {
+    public static void main(String[] args) {
+        Persona v[] = {
+            new Desempleado(),
+            new EstudianteCCFF(),
+            new EstudianteCCFF(),
+            new EstudianteCCFF(),
+            new Desempleado(),
+            new Desempleado(),
+            new EstudianteCCFF(),
+            new Desempleado(),
+            new Desempleado(),
+            new EstudianteCCFF()
+        };
+        darFormacion(v);
+    }
+    //Anyadir aqu� el m�todo darFormacion que se pide en el enunciado
+    public static void darFormacion(Persona[] v){
+    	for (int i = 0; i < v.length; i++) {
+    		((Formable)v[i]).formar(100);
+		}
+    }	
+
+    }
+
