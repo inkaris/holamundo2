a2447303d5822cf26795f4e00aea40d0d7aa2b20
danisir <daniel30>
2015-04-23 13:26:26 +0200

    sfvdfjh

--- exposiciondeed/src/Holamundo/holamundo.java
+++ exposiciondeed/src/Holamundo/holamundo.java
@@ -1,32 +1,14 @@
-package ejercicio3;
+package Holamundo;
 
-import ejercicio3.Desempleado;
-import ejercicio3.EstudianteCCFF;
-import ejercicio3.Formable;
-import ejercicio3.Persona;
-
-public class DarFormacion {
-    public static void main(String[] args) {
-        Persona v[] = {
-            new Desempleado(),
-            new EstudianteCCFF(),
-            new EstudianteCCFF(),
-            new EstudianteCCFF(),
-            new Desempleado(),
-            new Desempleado(),
-            new EstudianteCCFF(),
-            new Desempleado(),
-            new Desempleado(),
-            new EstudianteCCFF()
-        };
-        darFormacion(v);
-    }
-    //Anyadir aqu� el m�todo darFormacion que se pide en el enunciado
-    public static void darFormacion(Persona[] v){
-    	for (int i = 0; i < v.length; i++) {
-    		((Formable)v[i]).formar(100);
-		}
-    }	
-
-    }
-
+public class holamundo {
+	public static void main(String[] args) {
+		System.out.println("hola DAM!!!!");
+		System.out.println("");
+		System.out.println("E");
+	}
+	
+	public static void masSYSOS(){
+		System.out.println("jhdfjkgkjf");
+		System.out.println("kjdfjkhgfkjdrkjfdkjfjhgfj");
+	}
+}
